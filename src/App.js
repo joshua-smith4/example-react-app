6eb65051c3d19ac595e6fe9358440c65d2a15d31
Joshua Smith <joshua.smith@campbellsci.com>
2021-03-06 00:48:26 -0700

    implement higher-order-component for spacing tiles

--- src/App.js
+++ src/App.js
@@ -1,56 +1,69 @@
 import { useState, useEffect } from "react";
 
-function cards(numCards, width, margin) {
-    let retVal = [];
-    for (let i = 0; i < numCards; ++i) {
-        retVal.push(
-            <div
-                style={{
-                    backgroundColor: "green",
-                    margin: margin,
-                    width: width,
-                    height: 300,
-                }}
-            ></div>
-        );
-    }
-    return retVal;
-}
+function withWindowScalingTiles(component_generator) {
+    const components = component_generator();
+    return ({ margin, minWidth, minHeight }) => {
+        const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-function App() {
-    const minCardWidth = 220;
-    const margin = 10;
+        let numColumns = Math.floor(windowWidth / (minWidth + margin * 2));
+        if (numColumns <= 0) numColumns = 1;
+        const totalMargin = numColumns * margin * 2 + 1;
+        const cardWidth = (windowWidth - totalMargin) / numColumns;
 
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+        function onWindowResize(evt) {
+            setWindowWidth(window.innerWidth);
+        }
 
-    let numColumns = Math.floor(windowWidth / minCardWidth);
-    if (numColumns <= 0) numColumns = 1;
-    const totalMargin = numColumns * margin * 2 + 1;
-    const cardWidth = (windowWidth - totalMargin) / numColumns;
+        useEffect(() => {
+            window.addEventListener("resize", onWindowResize);
+            return () => {
+                window.removeEventListener("resize", onWindowResize);
+            };
+        }, []);
 
-    function onWindowResize(evt) {
-        setWindowWidth(window.innerWidth);
-    }
-
-    useEffect(() => {
-        window.addEventListener("resize", onWindowResize);
-        return () => {
-            window.removeEventListener("resize", onWindowResize);
-        };
-    }, []);
-
-    return (
-        <div>
+        return (
             <div
                 style={{
                     display: "flex",
                     flexWrap: "wrap",
                 }}
             >
-                {cards(10, cardWidth, margin)}
+                {components.map((TileComp, key) => {
+                    return (
+                        <TileComp
+                            key={key}
+                            width={cardWidth}
+                            minHeight={minHeight}
+                            margin={margin}
+                        />
+                    );
+                })}
             </div>
-        </div>
-    );
+        );
+    };
+}
+
+function cards() {
+    let retVal = [];
+    for (let i = 0; i < 10; ++i) {
+        retVal.push(({ width, minHeight, margin }) => (
+            <div
+                style={{
+                    backgroundColor: "green",
+                    width: width,
+                    minHeight: minHeight,
+                    margin: margin,
+                }}
+            ></div>
+        ));
+    }
+    return retVal;
+}
+
+const TiledDivs = withWindowScalingTiles(cards);
+
+function App() {
+    return <TiledDivs margin={10} minWidth={220} minHeight={300} />;
 }
 
 export default App;
